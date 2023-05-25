6eb899b5352bfda42e2d7e267ecd021239b46481
Harith Al-Safi <harith.alsafi@gmail.com>
2023-05-26 01:06:02 +0300

    removed main watermark

--- src/components/sidebar/components/Content.tsx
+++ src/components/sidebar/components/Content.tsx
@@ -3,7 +3,7 @@ import { Box, Flex, Stack } from '@chakra-ui/react';
 //   Custom components
 import Brand from 'components/sidebar/components/Brand';
 import Links from 'components/sidebar/components/Links';
-import SidebarCard from 'components/sidebar/components/SidebarCard';
+// import SidebarCard from 'components/sidebar/components/SidebarCard';
 
 // FUNCTIONS
 
@@ -20,7 +20,7 @@ function SidebarContent(props: { routes: RoutesType[] }) {
 			</Stack>
 
 			<Box ps='20px' pe={{ lg: '16px', '2xl': '20px' }} mt='60px' mb='40px' borderRadius='30px'>
-				<SidebarCard />
+				{/* <SidebarCard /> */}
 			</Box>
 		</Flex>
 	);
